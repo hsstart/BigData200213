b12869b92948c6119ef35ddee4eb9461141daa47
hs <279703534@qq.com>
2020-07-16 11:55:56 +0800

    dugu999_1

--- Git/src/main/java/com/atguigu/huashan/Dugujianfa.java
+++ Git/src/main/java/com/atguigu/huashan/Dugujianfa.java
@@ -10,6 +10,8 @@ public class Dugujianfa {
         System.out.println("dugu999999jian");
         System.out.println("dugu999999jian");
         System.out.println("dugu999999jian");
-        //红色：未追踪代码  绿色：暂存区
+        // todo  红色：未追踪代码    绿色：未追踪代码首次进暂存区
+        // todo  黑色：已提交本地库  蓝色：已提交本地库后，又发生了修改且未提交
+
     }
 }
