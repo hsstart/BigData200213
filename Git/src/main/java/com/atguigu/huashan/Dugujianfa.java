7f8ab174cac18363ae05dafd8574a6618b13bcf1
hs <279703534@qq.com>
2020-07-16 11:47:02 +0800

    dugu999

---
+++ Git/src/main/java/com/atguigu/huashan/Dugujianfa.java
@@ -0,0 +1,15 @@
+package com.atguigu.huashan;
+
+/**
+ * @author hsstart
+ * @create 2020-07-16 11:06
+ */
+public class Dugujianfa {
+    public static void main(String[] args) {
+        System.out.println("dugu999999jian");
+        System.out.println("dugu999999jian");
+        System.out.println("dugu999999jian");
+        System.out.println("dugu999999jian");
+        //红色：未追踪代码  绿色：暂存区
+    }
+}
